e99fa3eba28db61d49223f4a4342d0be3de96a8f
minjeongHEO <hmjoung9770@gmail.com>
2024-05-02 18:17:56 +0900

    refactor: 투두리스트 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import TodoList from './components/TodoList/TodoList';
 import Header from './components/Header/Header';
-import { v4 as uuidv4 } from 'uuid';
+import TodoListMain from './components/TodoList/TodoListMain';
 
 const filters = ['All', 'Active', 'Completed'];
+
 export default function App() {
     const getInitialTodo = () => {
         const savedTodo = localStorage.getItem('todoItem');
@@ -12,58 +12,6 @@ export default function App() {
     };
     const [todoItem, setTodoItem] = useState(getInitialTodo);
     const [filterType, setFilterType] = useState('All');
-    const [input, setInput] = useState('');
-
-    const handleChange = ({ target }) => {
-        const { value } = target;
-        setInput(value);
-    };
-
-    // Add
-    const addTodoList = () => {
-        if (input.trim() === '') {
-            setInput('');
-            return;
-        }
-        // const id = randomId();
-        const id = uuidv4();
-        setTodoItem((prev) => ({ ...prev, active: [...prev.active, { id, value: input }] }));
-        setInput('');
-    };
-
-    const toCompleted = (targetId, targetValue) => {
-        setTodoItem((prev) => ({
-            active: prev.active.filter(({ id }) => id !== targetId),
-            completed: [...prev.completed, { id: targetId, value: targetValue }],
-        }));
-    };
-    const toActive = (targetId, targetValue) => {
-        setTodoItem((prev) => ({
-            active: [...prev.active, { id: targetId, value: targetValue }],
-            completed: prev.completed.filter(({ id }) => id !== targetId),
-        }));
-    };
-
-    // 체크박스
-    const changeCheckBox = ({ target }) => {
-        const targetId = target.id;
-        const targetValue = target.value;
-        if (target.checked) toCompleted(targetId, targetValue);
-        if (!target.checked) toActive(targetId, targetValue);
-    };
-
-    const deleteItem = (targetId) => {
-        if (todoItem.active.some(({ id }) => id === targetId)) {
-            setTodoItem((prev) => ({ ...prev, active: prev.active.filter(({ id }) => id !== targetId) }));
-            return;
-        }
-
-        setTodoItem((prev) => ({ ...prev, completed: prev.completed.filter(({ id }) => id !== targetId) }));
-    };
-
-    const activeEnter = ({ key }) => {
-        if (key === 'Enter') addTodoList();
-    };
 
     useEffect(() => {
         localStorage.setItem('todoItem', JSON.stringify(todoItem));
@@ -73,19 +21,7 @@ export default function App() {
         <div className="container">
             <Header filters={filters} filterType={filterType} setFilterType={setFilterType} />
             <main>
-                <ul className="main_list">
-                    {(filterType === 'All' || filterType === 'Active') && (
-                        <TodoList todoItem={todoItem} deleteItem={deleteItem} changeCheckBox={changeCheckBox} type={'active'} />
-                    )}
-                    {(filterType === 'All' || filterType === 'Completed') && (
-                        <TodoList todoItem={todoItem} deleteItem={deleteItem} changeCheckBox={changeCheckBox} type={'completed'} />
-                    )}
-                </ul>
-
-                <div className="input_container">
-                    <input type="text" onChange={handleChange} value={input} onKeyDown={(e) => activeEnter(e)} placeholder="📓 MY TODO LIST"></input>
-                    <button onClick={addTodoList}>Add</button>
-                </div>
+                <TodoListMain filterType={filterType} todoItem={todoItem} setTodoItem={setTodoItem} />
             </main>
         </div>
     );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 
-export default function TodoList({ todoItem, deleteItem, changeCheckBox, type }) {
+export default function TodoList({ todoItem, setTodoItem, type }) {
     const items = todoItem[type];
 
     if (!items || items.length === 0) return null;
+
+    const deleteItem = (targetId) => {
+        if (todoItem.active.some(({ id }) => id === targetId)) {
+            setTodoItem((prev) => ({ ...prev, active: prev.active.filter(({ id }) => id !== targetId) }));
+            return;
+        }
+
+        setTodoItem((prev) => ({ ...prev, completed: prev.completed.filter(({ id }) => id !== targetId) }));
+    };
+
+    const toCompleted = (targetId, targetValue) => {
+        setTodoItem((prev) => ({
+            active: prev.active.filter(({ id }) => id !== targetId),
+            completed: [...prev.completed, { id: targetId, value: targetValue }],
+        }));
+    };
+    const toActive = (targetId, targetValue) => {
+        setTodoItem((prev) => ({
+            active: [...prev.active, { id: targetId, value: targetValue }],
+            completed: prev.completed.filter(({ id }) => id !== targetId),
+        }));
+    };
+    // 체크박스
+    const changeCheckBox = ({ target }) => {
+        const targetId = target.id;
+        const targetValue = target.value;
+        if (target.checked) toCompleted(targetId, targetValue);
+        if (!target.checked) toActive(targetId, targetValue);
+    };
 
     return (
         <>
